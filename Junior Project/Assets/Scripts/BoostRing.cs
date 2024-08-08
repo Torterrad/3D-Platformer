9595aedc23b5811eebea6c0597ad8f565a1e3de0
Torterrad <64802272+Torterrad@users.noreply.github.com>
2024-08-08 16:13:02 +0100

    Boost rings implemented
    Boost rings now launch the player in the rings forward rotation

--- Junior Project/Assets/Scripts/BoostRing.cs
+++ Junior Project/Assets/Scripts/BoostRing.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class BoostRing : MonoBehaviour
 {
+
+    private Rigidbody playerrb;
+
+    public int launchStrength;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerrb = GameObject.Find("Player").GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Player collided with ring");
+
+            Vector3 launchDirection = transform.forward;
+
+            StartCoroutine(ApplyForceOverTime(launchDirection));
+
+        }
+    }
+
+    IEnumerator ApplyForceOverTime(Vector3 direction)
+    {
+        Debug.DrawRay(transform.position, direction * 100, Color.red, 8f);
+        float duration = 0.75f;
+        float elapsedTime = 0f;
+
+        while(elapsedTime < duration)
+        {
+            //calculates the decreasing force over time
+            float forceMultiplier = Mathf.Lerp(1f, 0f, elapsedTime / duration);
+            //sets launch this frame to launch strength * decay/force decrease
+            float forceThisFrame = launchStrength * forceMultiplier;
+
+            //adds force to launch player in the forward direction * the force this frame
+            // playerrb.AddForce(direction.x, direction.y , direction.z * forceThisFrame, ForceMode.Impulse);
+            playerrb.velocity = (direction * forceThisFrame);
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
     }
 }
